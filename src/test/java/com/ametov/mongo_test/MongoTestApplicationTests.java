27a7910c7710e38210b04b135929bdb722256ca1
Ametov <ametov1st@gmail.com>
2024-12-23 14:17:02 +0600

    Spring init

---
+++ src/test/java/com/ametov/mongo_test/MongoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ametov.mongo_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
