0618a1a1e1a56262727f4c9eb814c777d136c8f0
Denis Ametov <dametov@in.nurtelecom.kg>
2025-01-08 14:30:07 +0600

    added filter by query param in find all

--- src/main/java/com/ametov/mongo_test/user/controller/UserApiController.java
+++ src/main/java/com/ametov/mongo_test/user/controller/UserApiController.java
@@ -9,9 +9,7 @@ import com.ametov.mongo_test.user.repository.UserRepository;
 import com.ametov.mongo_test.user.routes.UserRoutes;
 import lombok.AllArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -53,9 +51,19 @@ public class UserApiController {
 
     @GetMapping(UserRoutes.SEARCH)
     public List<UserResponse> search(@RequestParam(defaultValue = "0") Integer page,
-                                     @RequestParam(defaultValue = "10") Integer size) {
+                                     @RequestParam(defaultValue = "10") Integer size,
+                                     @RequestParam(defaultValue = "") String query) {
         Pageable pageable = PageRequest.of(page, size);
-        Page<UserDoc> users = userRepository.findAll(pageable);
+
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
+                .withMatcher("lastName", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+                .withMatcher("firstName", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+
+        Example<UserDoc> example = Example.of(
+                UserDoc.builder().lastName(query).firstName(query).build(), exampleMatcher
+        );
+
+        Page<UserDoc> users = userRepository.findAll(example, pageable);
 
         return users.getContent().stream().map(UserResponse::of).collect(Collectors.toList());
     }
